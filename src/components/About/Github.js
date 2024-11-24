9e9f3ff29d09725ad48920650a3f2406695dfc50
Mahanyasa Keesara <72213008+Mahanyasa@users.noreply.github.com>
2024-11-24 17:30:17 +0530

    Update Github.js

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -12,7 +12,7 @@ function Github() {
         username="Mahanyasa"
         blockSize={15}
         blockMargin={5}
-        color="#c084f5"
+        color="#6dbf4c"
         fontSize={16}
       />
     </Row>
